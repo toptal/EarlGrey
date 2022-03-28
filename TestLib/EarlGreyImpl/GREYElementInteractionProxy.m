cf0f9d969baf876c0f0ddeca1e46cd02a704c232
Aditya Atul Tirodkar <tirodkar@google.com>
2022-03-28 16:55:40 -0700

    Xcode 13.3 break fix
    PiperOrigin-RevId: 437887948

--- TestLib/EarlGreyImpl/GREYElementInteractionProxy.m
+++ TestLib/EarlGreyImpl/GREYElementInteractionProxy.m
@@ -115,9 +115,8 @@
 }
 
 - (id<GREYInteraction>)includeStatusBar {
-  __block id<GREYInteraction> includeStatusBar;
   GREYExecuteSyncBlockInBackgroundQueue(^{
-    includeStatusBar = [self->_remoteElementInteraction includeStatusBar];
+    [self->_remoteElementInteraction includeStatusBar];
   });
   return self;
 }
